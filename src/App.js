353229c953e442e87f61f7f74300f96f236b531e
Angie Spears <angie.spears809@gmail.com>
2018-08-24 13:51:42 -0400

    set up api fech and link to form

--- src/App.js
+++ src/App.js
@@ -3,12 +3,70 @@ import Header from "./components/Header/Header"
 import Form from "./components/Form/Form"
 import './App.css';
 
+const API_KEY = 'd0c74e4757653dd7d26fe20813b354d8'
+
 class App extends Component {
+  state = {
+    city: undefined,
+    country: undefined,
+    icon: undefined,
+    description: undefined,
+    temperature: undefined,
+    temp_max: undefined,
+    temp_min: undefined, 
+    sunrise: undefined,
+    sunset: undefined,
+    humidity: undefined,
+    wind: undefined, 
+    error: undefined
+  }
+  getWeather = async e => {
+    e.preventDefault()
+    const city = e.target.elements.city.value
+    const country = e.target.elements.country.value
+    const api_call = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`
+    )
+    const data = await api_call.json()
+    if (city && country) {
+      console.log(data)
+      this.setState({
+        city: data.name,
+        country: data.sys.country,
+        icon: data.weather[0].icon,
+        description: data.weather[0].description,
+        temperature: data.main.temp,
+        temp_max: data.main.temp_max,
+        temp_min: data.main.temp_min,
+        sunrise: data.sys.sunrise,
+        sunset: data.sys.sunset,
+        humidity: data.main.humidity,
+        wind: data.wind.speed,
+        error: ''
+      })
+    } else {
+      this.setState({
+        city: undefined,
+        country: undefined,
+        icon: undefined,
+        description: undefined,
+        temperature: undefined,
+        temp_max: undefined,
+        temp_min: undefined,
+        sunrise: undefined,
+        sunset: undefined, 
+        wind: undefined,
+        humidity: undefined,
+        error: 'Please enter the value'
+      })
+    }
+  }
+
   render() {
     return (
       <div className="App">
       <Header/>
-      <Form/>
+      <Form getWeather={this.getWeather}/>
       </div>
     );
   }
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -4,7 +4,7 @@ import React, {Component} from 'react'
 class Form extends Component {
     render() {
         return(
-            <form>
+            <form onSubmit={this.props.getWeather}>
 				<input type="text" name="city" placeholder="City..." />
 				<input type="text" name="country" placeholder="Country..." />
 				<button> Check Weather </button>
@@ -12,7 +12,7 @@ class Form extends Component {
         )
     }
 
-
+    
 }
 
 
